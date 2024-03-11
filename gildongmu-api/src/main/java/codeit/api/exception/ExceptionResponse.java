c81dc7dcf1e483cf069dae9d55e416939d0da9ce
haden <80521474+thdefn@users.noreply.github.com>
2024-03-11 15:44:49 +0900

    build: build project structure
    - create a multi-module based project structure
    - add components for handling exception
    - divide environment between develop and local

--- gildongmu-api/src/main/java/codeit/api/exception/ExceptionResponse.java
+++ gildongmu-api/src/main/java/codeit/api/exception/ExceptionResponse.java
@@ -7,12 +7,12 @@ import java.time.LocalDateTime;
 
 @ToString
 @Getter
-public class ExceptionResponse {
+public class ExceptionResponse<T> {
     private final String code;
-    private final String message;
+    private final T message;
     private final LocalDateTime timestamp;
 
-    public ExceptionResponse(String code, String message) {
+    public ExceptionResponse(String code, T message) {
         this.code = code;
         this.message = message;
         this.timestamp = LocalDateTime.now();
--- gildongmu-api/src/main/java/codeit/api/exception/GlobalExceptionHandler.java
+++ gildongmu-api/src/main/java/codeit/api/exception/GlobalExceptionHandler.java
@@ -13,16 +13,16 @@ public class GlobalExceptionHandler {
     private static final String LOG_FORMAT = "Class : {}, Code : {}, Message : {}";
 
     @ExceptionHandler(MockException.class)
-    public ResponseEntity<ExceptionResponse> handleMockException(MockException e) {
+    public ResponseEntity<ExceptionResponse<String>> handleMockException(MockException e) {
         log.info(LOG_FORMAT, e.getClass().getSimpleName(), e.getErrorCode(), e.getErrorCode().getMessage());
         return ResponseEntity.status(e.getErrorCode().getHttpStatus())
-                .body(new ExceptionResponse(e.getErrorCode().name(), e.getErrorCode().getMessage()));
+                .body(new ExceptionResponse<>(e.getErrorCode().name(), e.getErrorCode().getMessage()));
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ExceptionResponse> handleException(Exception e) {
+    public ResponseEntity<ExceptionResponse<String>> handleException(Exception e) {
         log.error(LOG_FORMAT, e.getClass().getSimpleName(), HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
         return ResponseEntity.internalServerError()
-                .body(new ExceptionResponse(HttpStatus.INTERNAL_SERVER_ERROR.name(), e.getMessage()));
+                .body(new ExceptionResponse<>(HttpStatus.INTERNAL_SERVER_ERROR.name(), e.getMessage()));
     }
 }
