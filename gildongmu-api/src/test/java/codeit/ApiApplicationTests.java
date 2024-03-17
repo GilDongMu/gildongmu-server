d1744383bd66f97b5db353d51e4e32521a9ecf7b
haden <80521474+thdefn@users.noreply.github.com>
2024-03-17 09:51:13 +0900

    test: test google oauth2 login feature

--- gildongmu-api/src/test/java/codeit/ApiApplicationTests.java
+++ gildongmu-api/src/test/java/codeit/ApiApplicationTests.java
@@ -1,15 +1,19 @@
 package codeit;
 
+import codeit.api.config.SecurityConfig;
 import codeit.common.security.JwtTokenManager;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 
 @SpringBootTest
 class ApiApplicationTests {
 
 	@MockBean
 	JwtTokenManager jwtTokenManager;
+	@MockBean
+	ClientRegistrationRepository clientRegistrationRepository;
 
 	@Test
 	void contextLoads() {
