60345ce841e1c34850bdcadc2142894cc3ff6f6c
haden <80521474+thdefn@users.noreply.github.com>
2024-03-19 10:01:54 +0900

    refactor: use implement instead unnecessary extend

--- gildongmu-common/src/main/java/codeit/common/security/OAuth2LoginUser.java
+++ gildongmu-common/src/main/java/codeit/common/security/OAuth2LoginUser.java
@@ -1,20 +1,47 @@
 package codeit.common.security;
 
+import codeit.domain.user.constant.Role;
 import codeit.domain.user.entity.User;
 import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 
-import java.util.Collections;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
 
-@Getter
-public class OAuth2LoginUser extends DefaultOAuth2User {
+public class OAuth2LoginUser implements OAuth2User {
+    @Getter
     private final User user;
+    private final List<GrantedAuthority> authorities;
+    private final Map<String, Object> attributes;
+    private final String nameAttributeKey;
 
     public OAuth2LoginUser(OAuth2User oAuth2User, String nameAttributeKey, User user) {
-        super(Collections.singleton(new SimpleGrantedAuthority(user.getRole().name())),
-                oAuth2User.getAttributes(), nameAttributeKey);
+        authorities = List.of(new SimpleGrantedAuthority(user.getRole().name()));
+        attributes = oAuth2User.getAttributes();
+        this.nameAttributeKey = nameAttributeKey;
         this.user = user;
+    }
+
+    public boolean hasAuthority(Role role){
+        return authorities.stream()
+                .anyMatch(grantedAuthority -> grantedAuthority.getAuthority().equals(role.name()));
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getName() {
+        return nameAttributeKey;
     }
 }
