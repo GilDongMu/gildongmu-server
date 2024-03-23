9c952d5594a4beeb1afa9232302ff3fcdf2e4f0e
haden <80521474+thdefn@users.noreply.github.com>
2024-03-24 08:17:51 +0900

    chore: add required dependency for integration test

--- gildongmu-chat/src/test/java/codeit/ChatApplicationTests.java
+++ gildongmu-chat/src/test/java/codeit/ChatApplicationTests.java
@@ -1,11 +1,14 @@
 package codeit;
 
+import codeit.common.security.JwtTokenManager;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 class ChatApplicationTests {
-
+    @MockBean
+    JwtTokenManager jwtTokenManager;
     @Test
     void contextLoads() {
     }
