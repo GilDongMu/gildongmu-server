d583fe6bc71a2a4941b20a10d43ccc2d18427a32
rekite <edward10379@gmail.com>
2024-03-25 00:23:34 +0900

    feat: add post request dto

---
+++ gildongmu-api/src/main/java/codeit/api/post/dto/request/PostUpdateRequest.java
@@ -0,0 +1,31 @@
+package codeit.api.post.dto.request;
+
+import codeit.api.post.dto.TripDate;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import java.util.List;
+
+public record PostUpdateRequest(
+    @NotBlank(message = "Title is required.")
+    String title,
+
+    @NotBlank(message = "Destination is required.")
+    String destination,
+
+    @NotNull(message = "Trip Date cannot be null")
+    List<TripDate> tripDate,
+
+    Short numberOfPeople,
+
+    @NotNull(message = "Member Gender cannot be null")
+    String gender,
+
+    @NotBlank(message = "Content is required.")
+    String content,
+
+    List<String> tag,
+
+    List<ImageCreateRequest> images
+) {
+
+}
