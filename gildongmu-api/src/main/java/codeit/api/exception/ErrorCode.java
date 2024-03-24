8dba8cf9ac3622ef5a8e347614ddf77081c24bbb
rekite <edward10379@gmail.com>
2024-03-25 00:12:14 +0900

    feat: add post errorcode

--- gildongmu-api/src/main/java/codeit/api/exception/ErrorCode.java
+++ gildongmu-api/src/main/java/codeit/api/exception/ErrorCode.java
@@ -12,7 +12,9 @@ public enum ErrorCode {
     // user
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당하는 유저가 없습니다."),
     ALREADY_REGISTERED_EMAIL(HttpStatus.BAD_REQUEST, "이미 사용중인 이메일입니다."),
-    PASSWORD_UNMATCHED(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다.");
+    PASSWORD_UNMATCHED(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다."),
+    //post
+    POST_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 게시글이 없습니다.");
     private final HttpStatus httpStatus;
     private final String message;
 }
