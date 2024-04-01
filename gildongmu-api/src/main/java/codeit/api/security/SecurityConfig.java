03a8946a53044494b1ea6e7b22cf4c8b7c83aaa4
rekite <edward10379@gmail.com>
2024-04-01 18:12:35 +0900

    chore: add security permit for search

--- gildongmu-api/src/main/java/codeit/api/security/SecurityConfig.java
+++ gildongmu-api/src/main/java/codeit/api/security/SecurityConfig.java
@@ -66,6 +66,7 @@ public class SecurityConfig {
                         authorize -> authorize
                                 .requestMatchers("/auth/**", "/swagger-ui/**", "/v3/api-docs/**", "/h2-console/**").permitAll()
                                 .requestMatchers(HttpMethod.GET, "/posts/**").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/search/**").permitAll()
                                 .requestMatchers("/oauth2/signup").hasRole("GUEST")
                                 .anyRequest().hasRole("USER")
                 )
